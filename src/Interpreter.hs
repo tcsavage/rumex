0de5cabef6e1c4de816988e1ed06a61b3e082a66
tcsavage <tcsavage@gmail.com>
2013-01-19 20:06:32 +0000

    Applying hlint suggestions

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -4,6 +4,7 @@ import Prelude hiding (Left, Right)
 import Tokens (tokenize)
 import Parser
 
+import Control.Monad
 import Control.Monad.Trans.State.Lazy
 import Data.Char
 import Data.List
@@ -24,16 +25,16 @@ interpretDebug inp progcode = printf "Output: %s\n\nFinal Tape:\n%s\n\nUnconsume
 		(out, (dt, inp, _, _)) = runState (runProg . parse . tokenize $ progcode) (initState inp)
 
 runProg :: Prog -> InterpreterState
-runProg (Join p1 p2) = (runProg p1) >> (runProg p2)
+runProg (Join p1 p2) = runProg p1 >> runProg p2
 runProg (CommandSequence c) = runCommand c
 runProg (Loop l) = do
-	(dt, inp, ft, out) <- get
+	(dt, _, _, out) <- get
 	case focus dt of
 		0 -> return out
-		otherwise -> runProg l >> (runProg $ Loop l)
+		otherwise -> runProg l >> runProg (Loop l)
 runProg (FunDef f) = do
 	(dt, inp, ft, out) <- get
-	let ft' = V.imap (\i v -> if i == (fromEnum $ focus dt) then Just f else v) ft
+	let ft' = V.imap (\i v -> if i == fromEnum (focus dt) then Just f else v) ft
 	put (dt, inp, ft', out)
 	return out
 runProg (Str s) = do
@@ -43,11 +44,11 @@ runProg (Str s) = do
 
 runCommand :: Command -> InterpreterState
 runCommand (Command n op) = do
-	out <- sequence . replicate n $ runOp op
+	out <- replicateM n $ runOp op
 	return $ last out
 
 runOp :: Op -> InterpreterState
-runOp Invoke = get >>= (\(dt, inp, ft, out) -> runProg $ fromJust $ ft ! (fromEnum $ focus dt))
+runOp Invoke = get >>= (\(dt, _, ft, _) -> runProg $ fromJust $ ft ! fromEnum (focus dt))
 runOp op = do
 	(dt, inp, ft, out) <- get
 	case op of
@@ -57,12 +58,12 @@ runOp op = do
 		Right -> put (r dt, inp, ft, out)
 		ReadChar -> doReadChar (dt, inp, ft, out)
 		WriteChar -> put (dt, inp, ft, out ++ [writeChar $ focus dt])
-		WriteInt -> put (dt, inp, ft, out ++ (writeInt $ focus dt))
+		WriteInt -> put (dt, inp, ft, out ++ writeInt (focus dt))
 	(_, _, _, out') <- get
 	return out'
 	where
-		doReadChar (dt, [], ft, out) = return ()
-		doReadChar (dt, (i:is), ft, out) = put (modFocus (const $ readChar i) dt, is, ft, out)
+		doReadChar (_, [], _, _) = return ()
+		doReadChar (dt, i:is, ft, out) = put (modFocus (const $ readChar i) dt, is, ft, out)
 
 type FunTable = V.Vector (Maybe Prog)
 
@@ -86,6 +87,7 @@ r (Tape _ _ []) = error "Reached end of tape"
 r (Tape a b (c:cs)) = Tape (b:a) c cs
 
 initTape :: [a] -> Tape a
+initTape [] = error "Cannot initialise an empty tape"
 initTape (x:xs) = Tape [] x xs
 
 instance Functor Tape where
@@ -94,7 +96,7 @@ instance Functor Tape where
 drawTape :: Show a => Int -> Tape a -> String
 drawTape n (Tape r v f) = printf "... %s  [%s]  %s ..." (drawList $ reverse $ take n r) (drawCell v) (drawList $ take n f)
 	where
-		drawList = concat . (intersperse "  ") . (map drawCell)
+		drawList = intercalate "  " . map drawCell
 		drawCell = show
 
 type DataTape = Tape Word8
@@ -109,7 +111,7 @@ writeChar :: Word8 -> Char
 writeChar = toEnum . fromEnum
 
 writeInt :: Word8 -> String
-writeInt = (filter isDigit) . show
+writeInt = filter isDigit . show
 
 initState :: String -> (DataTape, String, FunTable, String)
 initState inp = (initDataTape, inp, initFunTable, "")
