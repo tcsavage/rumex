6278822c59effd2ef30abe6f1e0ab0ce66b87ead
tcsavage <tcsavage@gmail.com>
2013-01-24 22:11:45 +0000

    Added some more robustness to usage display

--- src/Main.hs
+++ src/Main.hs
@@ -8,6 +8,9 @@ import Parser (parse)
 import System.Environment (getArgs)
 import Text.Show.Pretty (ppShow)
 
+usage :: String
+usage = "Usage:\n  Interpret: rumex i FILE [INPUT]\n  Compile: rumex c FILE OUTFILE\n  JIT: rumex jit FILE [INPUT]\n  Build AST: rumex ast FILE"
+
 main :: IO ()
 main = getArgs >>= run
 
@@ -16,24 +19,28 @@ run ("c":args) = c args
 run ("jit":args) = jit args
 run ("i":args) = i args
 run ("ast":args) = ast args
-run _ = putStrLn "Usage:\n  Interpret: rumex i FILE [INPUT]\n  Compile: rumex c FILE OUTFILE\n  JIT: rumex jit FILE\n  Build AST: rumex ast FILE"
+run _ = putStrLn usage
 
 c :: [String] -> IO ()
 c [file, out] = do
     src <- readFile file
     compile src out
+c _ = putStrLn usage
 
 jit :: [String] -> IO ()
 jit [file] = c [file, ""]
 jit [file, inp] = error "JIT not implemented"
+jit _ = putStrLn usage
 
 i :: [String] -> IO ()
 i [file] = i [file, ""]
 i [file, inp] = do
     src <- readFile file
     putStrLn $ interpret inp src
+i _ = putStrLn usage
 
 ast :: [String] -> IO ()
 ast [file] = do
     src <- readFile file
     putStrLn $ ppShow $ parse $ tokenize src
+ast _ = putStrLn usage
